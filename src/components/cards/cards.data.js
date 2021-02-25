a4cb9dea5ef9eddf0f99bc8356f9a534550b1d56
Steven De Avila <stevejd88@gmail.com>
2021-02-25 13:20:58 -0600

    Create data list for cards

---
+++ src/components/cards/cards.data.js
@@ -0,0 +1,85 @@
+import TonedThumb from '../../images/20-minutes-to-toned-thumb.jpg';
+import TonedTrainer from '../../images/20-minutes-to-toned-trainer.jpg';
+import CharlesThumb from '../../images/charles-race-thumb.jpg';
+import CharlesTrainer from '../../images/charles-race-trainer.jpg';
+import HiitThumb from '../../images/full-body-hiit-thumb.jpg';
+import HiitTrainer from '../../images/full-body-hiit-trainer.jpg';
+import RiverThumb from '../../images/kafue-river-thumb.jpg';
+import RiverTrainer from '../../images/kafue-river-trainer.jpg';
+import LakeThumb from '../../images/lake-inniscarra-thumb.jpg';
+import LakeTrainer from '../../images/lake-inniscarra-trainer.jpg';
+import PerformanceThumb from '../../images/performance-series-thumb.jpg';
+import PerformanceTrainer from '../../images/performance-series-trainer.jpg';
+import ShredThumb from '../../images/shred-and-burn-thumb.jpg';
+import ShredTrainer from '../../images/shred-and-burn-trainer.jpg';
+import SlowThumb from '../../images/slow-pulls-thumb.jpg';
+import SlowTrainer from '../../images/slow-pulls-trainer.jpg';
+
+const Users = [
+  {
+    id: 1,
+    headline: 'Lake Inniscarra, Ireland-- Pyramid',
+    time: '30:53',
+    views: '6,248',
+    thumbImg: LakeThumb,
+    trainerImg: LakeTrainer,
+    workouts: 0
+  },
+  {
+    id: 2,
+    headline: 'Performance Series',
+    thumbImg: PerformanceThumb,
+    trainerImg: PerformanceTrainer,
+    workouts: 9
+  },
+  {
+    id: 3,
+    headline: 'Slow Pulls and Fast Intervals',
+    time: '44:13',
+    views: '9,948',
+    thumbImg: SlowThumb,
+    trainerImg: SlowTrainer,
+    workouts: 0
+  },
+  {
+    id: 4,
+    headline: '20 Minutes to Toned',
+    thumbImg: TonedThumb,
+    trainerImg: TonedTrainer,
+    workouts: 12
+  },
+  {
+    id: 5,
+    headline: 'Charles Race, Boston, Massachussets',
+    time: '36:22',
+    views: '8,648',
+    thumbImg: CharlesThumb,
+    trainerImg: CharlesTrainer,
+    workouts: 0
+  },
+  {
+    id: 6,
+    headline: 'Full-Body HIIT Series',
+    thumbImg: HiitThumb,
+    trainerImg: HiitTrainer,
+    workouts: 12
+  },
+  {
+    id: 7,
+    headline: 'Kafue River, Zambia-- Power Strok Pyramid',
+    time: '22:22',
+    views: '4,660',
+    thumbImg: RiverThumb,
+    trainerImg: RiverTrainer,
+    workouts: 0
+  },
+  {
+    id: 8,
+    headline: 'Shred & Burn Series',
+    thumbImg: ShredThumb,
+    trainerImg: ShredTrainer,
+    workouts: 16
+  }
+];
+
+export default Users;
